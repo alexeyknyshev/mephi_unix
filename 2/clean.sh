5f53443009e305e079555207d9ad76b21de6e86e
Alexey <alexey.knyshev@gmail.com>
2015-03-09 00:36:55 +0300

    [2] Removed blocking input from clean.sh

--- 2/clean.sh
+++ 2/clean.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-echo "Press Enter to delete all created files and folders";
-read;
 rm -f $HOME/myfile;
 rm -f $HOME/mycopy;
 rm -rf $HOME/fruits;
 rm -rf $HOME/flowers;
 rm -rf $HOME/A;
 rm -f $HOME/test.txt;
-
 
 exit 0;
