ee821b0a496957caf02f23422d6d558ea3f0019e
KarKarbI4 <tvs.private@gmail.com>
2015-03-08 19:52:13 +0100

    4.7 added

---
+++ 4/7_newFileAlias.sh
@@ -0,0 +1,9 @@
+]#!/bin/bash
+shopt -s expand_aliases
+alias createfile='echo -n "Enter new file name : "
+read filename
+echo "Enter $filename content, then press CTRL+D to finish input : "
+cat > $filename'
+echo "now you're able to use createfile command!"
+echo "command : createfile"
+createfile
