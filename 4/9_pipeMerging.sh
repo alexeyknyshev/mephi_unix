a7c1f01e3036737d39e0de7fc05fa750e8644a20
Alexey <alexey.knyshev@gmail.com>
2015-03-09 00:22:55 +0300

    Fix typo in 4.9

--- 4/9_pipeMerging.sh
+++ 4/9_pipeMerging.sh
@@ -8,7 +8,7 @@ echo 'this is f2 content!' | cat > $HOME/f2
 echo 'command : cat $HOME/f2'
 cat $HOME/f2
 
-echo "let's merhge f1 and f2 to single f3 file!"
+echo "let's merge f1 and f2 to single f3 file!"
 cat $HOME/f1 $HOME/f2 > $HOME/f3
 echo 'command : cat $HOME/f3'
 cat $HOME/f3
